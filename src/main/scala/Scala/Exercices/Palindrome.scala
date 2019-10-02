e434a2a51812081072af7e729bc7a4730288cb84
Pierre Verbe <pierre.verbe@free.fr>
2019-10-02 17:30:59 +0200

    add exercice palindrome to the project

--- src/main/scala/Scala/Exercices/Palindrome.scala
+++ src/main/scala/Scala/Exercices/Palindrome.scala
@@ -1,39 +1,29 @@
 package Scala.Exercices
 
-object Palindrome {
+object Palindrome extends App{
 
   /*
   Palindrome : A word or group of words that can be read from left to right or right to left with the same meaning
   ex : kayak, bob
   */
 
+  //not palindrome
+  println("ThisThing is a palindrome ? " + isPalindrome("ThisThing"))
+
+  //palindrome
+  println("Kayak is a palindrome ? " + isPalindrome("Kayak"))
+
   def isPalindrome(word: String): Boolean = {
-    val wordLowerCase = word.toLowerCase
-    val length = wordLowerCase.length
-    var forward = 0
-    var backward = length - 1
-
-    while (backward > forward){
-      val forwardChar = wordLowerCase.charAt(forward)
-      val backwardChar = wordLowerCase.charAt(backward)
-
-      if (forwardChar != backwardChar) return false
-
-      backward -= 1
-      forward += 1
+    def isPalindromeRecursive(word: String, current: Int): Boolean = {
+      if (current > word.length/2.ceil.toInt) true
+      else if (word.charAt(current) != word.charAt(word.length-1-current)) false
+      else isPalindromeRecursive(word, current+1)
     }
-    println("the word " + wordLowerCase + " is a Palindrom")
-    true
-  }
 
-  def isPalindrome2(word: String): Boolean = {
     val wordLowerCase = word.toLowerCase
-    val length = wordLowerCase.length
+    val currentPosition = 0
 
-    for(i <- 0 to length/2.ceil.toInt) if (wordLowerCase.charAt(i) != wordLowerCase.charAt(length-i-1)) return false
-
-    true
+    isPalindromeRecursive(wordLowerCase, currentPosition)
   }
 
 }
-
---
+++ src/test/scala/Scala/Exercices/PalindromeTest.scala
@@ -0,0 +1,15 @@
+package Scala.Exercices
+
+import org.scalatest.{FunSuite, Matchers}
+
+class PalindromeTest extends FunSuite with Matchers {
+
+  test("ThisThing should return false because it's not a palindrome") {
+    Palindrome.isPalindrome("ThisThing") shouldBe false
+  }
+
+  test("Kayak should return true because it's a palindrome") {
+    Palindrome.isPalindrome("Kayak") shouldBe true
+  }
+
+}
